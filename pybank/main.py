9f0c3da89787bc0c9619d676412a0475007d23b7
Maria Dong <maria.j.dong@gmail.com>
2020-05-30 16:14:11 -0400

    (initial commit)

--- pybank/main.py
+++ pybank/main.py
@@ -2,5 +2,25 @@
 import os
 import csv
 
-with 
+#define list variables: months, profit/loss
+months = []
+profit_loss = []
+
+#specify budget csv path
+bankpath = os.path.join ("resources","budget_data.csv")
+
+#read budget csv file
+with open (bankpath) as bankfile:
+    bankreader = csv.reader (bankfile,delimiter = ',')
+    
+    #specify header
+    bankheader = next(bankreader)
+    
+    #begin loop through rows
+    for row in bankreader:
+        
+        #add months & profit/loss to existing list
+        months.append (row[0])
+        #maintain integer status for calculations later
+        profit_loss.append (int(row[1]))
 
